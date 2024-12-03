799d6b20c69817d124c226e77b2f073f2cbd7573
Wojciech Sarwinski <01133073@pw.edu.pl>
2024-12-03 21:05:35 +0100

    corrected emplace in TextureManager

--- src/source/TextureManager.cc
+++ src/source/TextureManager.cc
@@ -6,7 +6,10 @@ const Texture& TextureManager::get_texture(const std::string& identifier, SDL_Re
     auto it = texture_cache.find(identifier);
     if (it != texture_cache.end())
         return it->second;
-    auto emplaced = texture_cache.emplace(identifier, Texture(identifier, renderer));
+    auto emplaced = texture_cache.emplace(
+        std::piecewise_construct,
+        std::forward_as_tuple(identifier),
+        std::forward_as_tuple(identifier, renderer));
     if (emplaced.second) {
         return emplaced.first->second;
     }
@@ -15,7 +18,10 @@ const Texture& TextureManager::get_texture(const std::string& identifier, SDL_Re
 
 bool TextureManager::preloadTexture(const std::string &identifier, SDL_Renderer *renderer)
 {
-    auto emplaced = texture_cache.emplace(identifier, Texture(identifier, renderer));
+    auto emplaced = texture_cache.emplace(
+        std::piecewise_construct,
+        std::forward_as_tuple(identifier),
+        std::forward_as_tuple(identifier, renderer));
     bool& success = emplaced.second;
     return success;
 }
