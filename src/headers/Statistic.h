40ec7382626b3f03761301ecc30467ca95517285
Wojciech Sarwinski <01133073@pw.edu.pl>
2025-01-15 06:40:54 +0100

    implemented operator> method

--- src/headers/Statistic.h
+++ src/headers/Statistic.h
@@ -21,7 +21,7 @@ public:
     //bool operator==(const Statistic& other) const;
     //bool operator!=(const Statistic& other) const;
     //bool operator<(const Statistic& other) const;
-    //bool operator>(const Statistic& other) const;
+    bool operator>(const Statistic& other) const;
     //bool operator<=(const Statistic& other) const;
     //bool operator>=(const Statistic& other) const;
 };
