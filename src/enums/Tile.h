f1ca40fd8f2713c341239891ae4bca2b4af3ef6d
Wojciech Sarwinski <01133073@pw.edu.pl>
2025-01-15 02:18:37 +0100

    added Tile enum class

---
+++ src/enums/Tile.h
@@ -0,0 +1,13 @@
+/*
+ * File:        Tile.h
+ * Description: Identifies types of tiles on map.
+ *
+ * Author:      Wojciech Sarwiński
+ * 
+ * Date:        28.11.2024
+ */
+
+enum class Tile {
+    REACHABLE,
+    UNREACHABLE
+};
