718da5f89f215a31777f84a6fdeee6c71db72411
Wojciech Sarwinski <01133073@pw.edu.pl>
2025-01-25 12:03:44 +0100

    Battle Manager is clickable

--- src/headers/BattleManager.h
+++ src/headers/BattleManager.h
@@ -17,17 +17,19 @@
 #include "IManager.h"
 #include "HexMap.hpp"
 #include "FieldUnitRenderable.h"
+#include "IClickable.h"
+#include "InputController.h"
 
 class RendersVisitator;
 
 constexpr unsigned int BATTLE_HEX_WIDTH = 15;
 constexpr unsigned int BATTLE_HEX_HEIGHT = 11;
 
-class BattleManager : public IManager
+class BattleManager  : public IManager, public IClickable
 {
 public:
     BattleManager();
-    BattleManager(const Army& player_army, const Army& enemy_army, HexMap<Tile> map);
+    BattleManager(const Army& player_army, const Army& enemy_army, HexMap<Tile> map, std::weak_ptr<InputController> input_controller, std::function<void(Army*)> change_mode_function);
     const BattleField& getBattleField() const;
     const std::string& getBackground() const;
     Hex getBattleGridDimensions() const;
@@ -37,6 +39,9 @@ public:
     void makeMove(const UnitMove& unitMove);
     const std::vector<std::unique_ptr<FieldUnitRenderable>>& getAllUnits() const;
     void accept(RendersVisitator& visitor) const override;
+    virtual void reactToClick(bool left_button, const Hex& click_position);
+private:
+    void makeRenderable();
 private:
     BattleField field_;
     HexMap<Tile> map_;
@@ -44,4 +49,7 @@ private:
     std::vector<std::unique_ptr<FieldUnitRenderable>> renderable_units_;
     std::unordered_map<UnitType, std::string> texture_idle_;
     std::unordered_map<UnitType, std::string> texture_dead_;
+
+    std::weak_ptr<InputController> input_controller_;
+    std::function<void(Army*)> change_mode_function_;
 };
